65e2484056bd904b4cd48277c1e4f84c62dd7eeb
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-09-28 13:07:08 -0600

    Initial Commit

---
+++ XAFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 28/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XAFit/XAFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XAFitApp.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct XAFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
