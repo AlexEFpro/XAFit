e3040687023f51c069047fbb6b5d8a5ea4643408
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-09-29 20:26:02 -0600

    Se modifico el componente para hacerlo modular

---
+++ XAFit/Views/WeightPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  WeightPickerView.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 29/09/24.
+//
+
+import SwiftUI
+
+struct WeightPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeightPickerView()
+}
