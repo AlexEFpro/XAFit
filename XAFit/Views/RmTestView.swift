c2bc7cfd2e5b17c92211cc29caf031e47b2c4855
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-01 18:12:17 -0600

    se modifico el modelo para que se implemente correctamente el protocolo desarrollado para hacer generico el timer

---
+++ XAFit/Views/RmTestView.swift
@@ -0,0 +1,18 @@
+//
+//  RmTestView.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 30/09/24.
+//
+
+import SwiftUI
+
+struct RmTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RmTestView()
+}
