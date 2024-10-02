b88f0bdafccaf383b17be6d7d0788af241d3ef27
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-01 18:13:56 -0600

    version final de los metodos del modelo y actualizacion del cronoprotocol

--- XAFit/Protocols/CronoProtocol.swift
+++ XAFit/Protocols/CronoProtocol.swift
@@ -10,5 +10,12 @@ protocol CronoProtocol: ObservableObject{
     var displayValue : Double {get set}
     var showValue : Bool {get set}
     var value : Double {get set}
+    var displayDigits : Double {get set}
+    var setTime : Double{get set}
+    var timer : Timer? {get set}
+    var isPaused : Bool {get set}
     func startTimer()
+    func stopTimer()
+    func resumeTimer()
+    func pauseTimer()
 }
--- XAFit/components/TimerView.swift
+++ XAFit/components/TimerView.swift
@@ -7,91 +7,88 @@
 
 import SwiftUI
 
-    
+
 struct TimerView<Model:CronoProtocol>: View {
-    @ObservedObject var tm = Model
+    @ObservedObject var tm : Model
     
-        var body: some View {
-            ZStack{
-                //level1  external background
-                Circle()
-                    .stroke(lineWidth: 24)
-                    .frame(width: 200, height:200 )
-                    .foregroundStyle(.white)
-                    .shadow(color: Color.blue.opacity(0.1), radius: 10 , x:10, y:10)
-                //level2 inernal background
-                Circle()
-                    .stroke(lineWidth: 0.34)
-                    .frame(width: 175, height: 175)
-                    .foregroundStyle(LinearGradient(
-                        gradient: Gradient(colors: [.green .opacity(0.3), .blue]),
-                        startPoint: .bottomTrailing,
-                        endPoint: .topLeading))
-                    .overlay{
-                        //level 3 sahdow background
-                        Circle()
-                            .stroke( .red.opacity(0.1), lineWidth:2)
-                            .blur(radius: 5)
-                            .mask{
-                                Circle()
-                                    .foregroundStyle(LinearGradient(
-                                        gradient: Gradient(colors: [.green , .blue]),
-                                        startPoint: .topLeading,
-                                        endPoint: .bottomTrailing))
-                            }
-                    }
-                // level 4 progress bar
-                Circle()
-                    .trim(from: 0, to: tm.showValue ? tm.value : 0)
-                    .stroke(style:StrokeStyle(lineWidth: 24 , lineCap:.round ))
-                    .frame(width: 200 , height: 200)
-                    .rotationEffect(.degrees(-90))
-                    .foregroundStyle(LinearGradient(
-                        gradient: Gradient(colors: [.green , .blue]),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing))
-                NumValue(displayValue: tm.displayValue, color: .blue)
-                
-            }
-            .onTapGesture { //play function
-                withAnimation(.spring().speed(0.2)){
-                    tm.showValue.toggle()
-                    if tm.showValue{
-                        Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true){ timer in
-                            if tm.displayValue < tm.value{
-                                tm.displayValue += 0.01
-                            }else{
-                                timer.invalidate()
-                                
-                            }
+    var body: some View {
+        ZStack{
+            //level1  external background
+            Circle()
+                .stroke(lineWidth: 24)
+                .frame(width: 200, height:200 )
+                .foregroundStyle(.white)
+                .shadow(color: Color.blue.opacity(0.1), radius: 10 , x:10, y:10)
+            //level2 inernal background
+            Circle()
+                .stroke(lineWidth: 0.34)
+                .frame(width: 175, height: 175)
+                .foregroundStyle(LinearGradient(
+                    gradient: Gradient(colors: [.green .opacity(0.3), .blue]),
+                    startPoint: .bottomTrailing,
+                    endPoint: .topLeading))
+                .overlay{
+                    //level 3 sahdow background
+                    Circle()
+                        .stroke( .red.opacity(0.1), lineWidth:2)
+                        .blur(radius: 5)
+                        .mask{
+                            Circle()
+                                .foregroundStyle(LinearGradient(
+                                    gradient: Gradient(colors: [.green , .blue]),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing))
                         }
-                    }else{
-                        Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true){
-                            timer in
-                            if tm.displayValue > 0 {
-                                tm.displayValue -= 0.01
-                            }else{
-                                timer.invalidate()
-                            }
-                            
-                        }
-                    }
-                    
                 }
-            }
-        }
-    }
-    
-    #Preview {
-        TimerView()
-    }
-    
-    struct NumValue: View {
-        var displayValue = 0.0
-        var color : Color
-        var body: some View {
-            Text("\(Int(displayValue * 100))")
+            // level 4 progress bar
+            Circle()
+                .trim(from: 0, to: tm.showValue ? tm.displayValue : 0)
+                .stroke(style:StrokeStyle(lineWidth: 24 , lineCap:.round ))
+                .frame(width: 200 , height: 200)
+                .rotationEffect(.degrees(-90))
+                .foregroundStyle(LinearGradient(
+                    gradient: Gradient(colors: [.green , .blue]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing))
+            Text("\(Int(tm.displayDigits))")
                 .font(.largeTitle.bold())
                 .foregroundStyle(.blue)
+            
+            
         }
+        .padding(.bottom,45)
+        HStack{
+            
+            Button("", systemImage: "play" , action: {
+                if tm.isPaused {
+                    tm.resumeTimer()
+                } else {
+                    tm.startTimer()
+                }
+            })
+            .font(.title.bold())
+            .foregroundStyle(.blue)
+            
+            Button("", systemImage: "stop", action: tm.stopTimer)
+                .font(.title.bold())
+                .foregroundStyle(.blue)
+            
+            Button("", systemImage: "pause", action: tm.pauseTimer)
+                .font(.title.bold())
+                .foregroundStyle(.blue)
+            
+            
+            
+            
+            
+        }
+        
+        
     }
+    
+}
+
+#Preview {
+    TimerView(tm: TimeManager())
+}
+
