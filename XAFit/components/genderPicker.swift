00d532d5c3f6e4f7dcc79e15e67483735b5b8bee
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-03 07:37:38 -0600

    integración de los pickers age and gender

---
+++ XAFit/components/genderPicker.swift
@@ -0,0 +1,18 @@
+//
+//  genderPicker.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 03/10/24.
+//
+
+import SwiftUI
+
+struct genderPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    genderPicker()
+}
