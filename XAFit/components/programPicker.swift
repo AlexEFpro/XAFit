86053d5d81acbced0f2c36587fad8a6f6625624c
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-06 20:30:29 -0600

    se avnzao de nueva cuenta en el componente testView, con la finalidad de establecer la vista para el desarrollo de la evaluación.

---
+++ XAFit/components/programPicker.swift
@@ -0,0 +1,18 @@
+//
+//  programPicker.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 06/10/24.
+//
+
+import SwiftUI
+
+struct programPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    programPicker()
+}
