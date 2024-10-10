7404bcc1eea756a9a1e2aa73f0a60399a1890757
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-10 16:43:31 -0600

    Se desarrolla la base para el requerimiento 2 creandose un button a travez del cual al terminar la sesión  agrega un sesion al historial del user

---
+++ XAFit/components/FinishButton.swift
@@ -0,0 +1,18 @@
+//
+//  FinishButton.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 10/10/24.
+//
+
+import SwiftUI
+
+struct FinishButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishButton()
+}
