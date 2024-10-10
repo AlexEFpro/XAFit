e52d5f95f9d0fab3b1271a82a62fff3f25f381bd
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-10 16:43:53 -0600

    actualizacion de finishbutton

--- XAFit/components/FinishButton.swift
+++ XAFit/components/FinishButton.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct FinishButton: View {
+    @State var workoutsEnds : Bool = false
+    @State var workouts : Int = 0
+    func countingWorkout(){
+        if workoutsEnds == true{
+            workouts += 1
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button( action: {
+            workoutsEnds = true
+            
+            
+        })
+        {
+            Text("Finish Workout")
+            
+        }
+        .buttonStyle(.borderedProminent)
+        .font(.largeTitle.bold())
+        Text(String("Workouts : \(workouts)"))
+            .font(.title.bold())
+            .alert("Are you sure to end the session?", isPresented: $workoutsEnds){
+                Button("Yes", action:countingWorkout)
+                Button("Cancel", role: .cancel){}
+            }
+        
+        
     }
 }
 
