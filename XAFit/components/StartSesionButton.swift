71b28cc1ad90355fb7fcec2f94d4738991621ed6
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-14 22:03:10 -0600

    Se modifico diferentes apartados de la UI entre los que resalta el cumplimiento del requerimiento 1 del sprint5 :  Los WheelPickers ocupaban un espacio excesivo en la interfaz de usuario, lo que comprometía la distribución visual de los demás elementos. El objetivo era reducir el espacio que estos componentes ocupan sin afectar su funcionalidad o usabilidad

---
+++ XAFit/components/StartSesionButton.swift
@@ -0,0 +1,18 @@
+//
+//  StartSesionButton.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 10/10/24.
+//
+
+import SwiftUI
+
+struct StartSesionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartSesionButton()
+}
