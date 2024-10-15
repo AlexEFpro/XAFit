10097658cc8d639a0cca5402c56364fb46cc01d1
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-10-15 12:18:05 -0600

    Se actualizo el model ProgramsandPhysical para agregar los entrenamientos diferenciados por segmento

---
+++ XAFit/components/SegmentTrainingPicker.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentTrainingPicker.swift
+//  XAFit
+//
+//  Created by Alejandro  Salinas Ramirez on 15/10/24.
+//
+
+import SwiftUI
+
+struct SegmentTrainingPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentTrainingPicker()
+}
